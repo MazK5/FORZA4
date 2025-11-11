7990de8fde465e1d4a6d8a2f95d9a4fc024356b6
MazK5 <maksymkhvorostovytskyi@gmail.com>
2025-11-11 01:07:30 +0100

    added css html basic logic

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,132 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+/*
+  ////////////////////////////////////////
+    FORZA4 TABLE TODO
+  /////////////////////////////////////////
+
+ -move from app.vue to views
+ -add code logic to make the game interactive
+    -add logic for winning
+    -add logic for turns
+    -add player vs player / bot
+    -animations when inserting elements ( it needs to fall down )
+    -color for different players and background (when empty = ,p1= red, p2= yellow )
+    -add restart button
+    -add score board
+
+  - save scores with pinia or cookies
+  -use router to create different views ( home, game, settings, about us )
+*/
+
+const elementInserted = ref(0) ;
+
+function insert() {
+  console.log("insert called");
+  elementInserted.value += 1 ;
+}
+
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
 
-<style scoped></style>
+  <h1>FORZA4 </h1>
+  <h2> By maksym </h2>
+  
+  <p class ="generalText">
+    Visit <a href="https://github.com/MazK5/FORZA4" target="_blank" rel="noopener">git project</a> to see the code repository.
+  </p>
+
+  <div id="forza4table">
+
+    <div class="tbleRow">
+      <!-- to change with a for i ? -->
+      <P @click="insert"></P>
+      <P @click="insert"></P>
+      <P @click="insert"></P>
+      <P @click="insert"></P>
+      <P @click="insert"></P>
+      <P @click="insert"></P>
+      <P @click="insert"></P>
+      <P @click="insert"></P>
+      <P @click="insert"></P>
+      <P @click="insert"></P>
+    </div>
+
+    <div class="tbleRow">
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+    </div>
+
+    <div class="tbleRow">
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+    </div>
+
+    <div class="tbleRow">
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+    </div>
+
+    <div class="tbleRow">
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+    </div>
+
+    <div class="tbleRow">
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+      <P>  </P>
+    </div>
+
+  </div>
+  <p id="count"> Elements inserted: {{ elementInserted }} </p>
+
+  <footer>
+    <p id="author">Author: Maksym <br>
+    <a href="mailto:"></a>Maksym@</p>
+  </footer>
+
+</template>
+<style scoped>
+</style>
